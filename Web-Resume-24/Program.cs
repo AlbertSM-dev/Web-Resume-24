4d6e180f11b689b7b8aa0b394d194106a709d786
AlbertSM-dev <lokdok1988@hotmail.com>
2024-03-15 20:42:51 +0000

    Add project files.

---
+++ Web-Resume-24/Program.cs
@@ -0,0 +1,17 @@
+namespace Web_Resume_24
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Web-Resume-24/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Web_Resume_24
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}

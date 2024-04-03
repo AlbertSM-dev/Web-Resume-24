9c51fdb8cc51461bd7b178f6fe1781e3c709aad0
AlbertSM-dev <lokdok1988@hotmail.com>
2024-04-03 18:09:08 +0100

    Add projects page

--- Web-Resume-24/Controllers/HomeController.cs
+++ Web-Resume-24/Controllers/HomeController.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Web_Resume_24.wwwroot.Controllers
+namespace Web_Resume_24.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
         {
             return View();
         }

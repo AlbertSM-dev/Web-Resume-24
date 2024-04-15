3098e98e0f6402fc71d68689ff1481af2179832d
AlbertSM-dev <lokdok1988@hotmail.com>
2024-04-15 21:08:41 +0200

    srp

---
+++ Web-Resume-24/Controllers/ProgrammingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Resume_24.Controllers
+{
+    public class ProgrammingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
